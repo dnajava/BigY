94882dfd04692e0104975fbf527b0fbadf6c48ae
Ilpo Kantonen <ilkant@users.noreply.github.com>
2021-04-10 01:52:15 +0300

    Update bigY2.py

--- bigY2.py
+++ bigY2.py
@@ -3,26 +3,26 @@ __date__ = "$9.4.2021 22:24:00$"
 
 from csv import reader
 
+__author__ = "Ilpo Kantonen"
+__date__ = "$10.4.2021 01:48:00$"
+
+from csv import reader
+
 def make_dictionary(fname_p: str):
-    tmp_dict = {"A": 0, "T": 0, "C": 0, "G": 0, "?": 0, "TOT": 0}   # Note that there can be double letters too.
+    tmp_dict = {"A": 0, "T": 0, "C": 0, "G": 0, "?": 0, "TOTAL": 0}
     r, v = 0, 0
     try:
         with open(fname_p, 'r') as read_obj:
             csv_reader = reader(read_obj)
             for k in csv_reader:
                 r += 1
-                tmp_dict["TOT"] += 1
-                # tmp_dict[k[6]] += 1   OR
-                if k[6] == "A":
-                    tmp_dict["A"] += 1
-                elif k[6] == "T":
-                    tmp_dict["T"] += 1
-                elif k[6] == "C":
-                    tmp_dict["C"] += 1
-                elif k[6] == "G":
-                    tmp_dict["G"] += 1
-                elif k[6] == "?":
-                    tmp_dict["?"] += 1
+                if r == 1:
+                    continue
+                tmp_dict["TOTAL"] += 1
+                if k[6] in tmp_dict.keys():
+                    tmp_dict[k[6]] += 1
+                else:
+                    tmp_dict[k[6]] = 1
     except (IOError, OSError) as err:
         print(err)
     finally:
@@ -31,12 +31,17 @@ def make_dictionary(fname_p: str):
     return tmp_dict
 
 if __name__ == '__main__':
-    path = '/home/sda4/Lataukset/'
-    files = ['KIT1_BigY_Data_20210408.csv', 'KIT2_BigY_Data_20210408.csv']
+    path = '/path/to/download/directory/'
+    files = ['KIT1_BigY_Data_YYYYMMDD.csv', 'KIT2_BigY_Data_YYYYMMDD.csv']
 
     for f in files:
         filen = path + f
         dic = make_dictionary(filen)
-        for di in dic:
-            print(di, dic[di], 'pieces and percentage', 100 * dic[di] / dic["TOT"])
+        if (dic["TOTAL"] > 0):
+            for di in dic:
+                if di != "TOTAL":
+                    print(di, dic[di], 'pieces and percentage', 100 * dic[di] / dic["TOTAL"])
+            print('TOTAL', dic["TOTAL"], 'pieces and percentage', 100 * dic[di] / dic["TOTAL"])
+        else:
+            print('There were no A, G, C, T or such in Big Y csv file!')
         print()
